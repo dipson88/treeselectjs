728fc1d91db061d4c5b5ba04687a3085336380ee
Dmitry Zhuravkov <d.zhuravkov@itransition.com>
2022-06-27 21:35:36 +0300

    Safari focus list item fix.

--- demo/todo.js
+++ demo/todo.js
@@ -1,5 +1,4 @@
 // IMPROVEMENTS
-// (DONE) add delta to list height (some data sometimes hidden at the bottom) mb innerHeight of the window?? (recheck, 45px delta was added)
 // (DONE) add event listener scroll only on y scroll if it is possible (probably it is not possible)
 // (DONE) to body width (fixed but looks like TODO)
 // should we show space for input on new line (unsearchable mode check) on RESIZE (try to set width and height to 0 by default and on focus chenge styles)
@@ -8,4 +7,4 @@
 // list scroll overlaps border 
 // updateDisabled methods to avoid a remount
 // create update function for the list to avoid a remount
-
+// use _ instead private class #
