adbd3d5bb0326f866e1dc1992b0735c8cff98a04
Dmitry Zhuravkov <d.zhuravkov@itransition.com>
2022-06-29 15:22:42 +0300

    Update todo.

--- demo/todo.js
+++ demo/todo.js
@@ -1,9 +1,7 @@
 // IMPROVEMENTS
-// (DONE) add event listener scroll only on y scroll if it is possible (probably it is not possible)
-// (DONE) to body width (fixed but looks like TODO)
 // should we show space for input on new line (unsearchable mode check) on RESIZE (try to set width and height to 0 by default and on focus chenge styles)
 // Recheck performance
-// Lagging during fast scroll with append to body
+// Lagging during fast scroll with append to body?
 // list scroll overlaps border 
 // updateDisabled methods to avoid a remount
 // create update function for the list to avoid a remount
