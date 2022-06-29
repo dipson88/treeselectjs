86f6a5c2ad03b5798234a02b8dc4e06aa1236d6f
Dmitry Zhuravkov <58396014+dipson88@users.noreply.github.com>
2022-06-29 18:26:26 +0400

    Bugfix/MinorBugFixesV0.2.7 (#10)
    * Remove misspelling for the direction attribute.
    
    * Add a new point about the same values to the todo.
    
    * Add Requests section to the todo.
    
    * Fix wrong list position on horizontal scrolling.
    
    * Update todo.
    
    * Add static list prop and remove mispellings.
    
    * Update dist.
    
    * Update TODO.
    
    Co-authored-by: Dmitry Zhuravkov <d.zhuravkov@itransition.com>

--- docs/todo.js
+++ docs/todo.js
@@ -1,11 +1,13 @@
 // IMPROVEMENTS
-// (DONE) add event listener scroll only on y scroll if it is possible (probably it is not possible)
-// (DONE) to body width (fixed but looks like TODO)
 // should we show space for input on new line (unsearchable mode check) on RESIZE (try to set width and height to 0 by default and on focus chenge styles)
 // Recheck performance
-// Lagging during fast scroll with append to body
+// Lagging during fast scroll with append to body?
 // list scroll overlaps border 
 // updateDisabled methods to avoid a remount
 // create update function for the list to avoid a remount
 // use _ instead private class #
 // disable change focused element on fast scroll appendToBody
+// Should we add a list direction if we use staticList prop?
+
+// REQUESTS
+// Investigate an ability to add a diff names but the same values, and check them.
