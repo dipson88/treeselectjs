acfe6b6af6afba594753ab1c888005ccbd5bee44
Dzmitry Zhuraukou <zhuravkov888@gmail.com>
2025-02-20 17:23:43 +0100

    Remove build files from the demo app.

--- demo/index.js
+++ demo/index.js
@@ -1,4 +1,10 @@
-import Treeselect from './static/treeselectjs.mjs'
+// Treeselect is available via the link in the index.html file (treeselectjs.umd.js)
+const Treeselect = globalThis.Treeselect
+
+if (!Treeselect) {
+  throw new Error('Treeselect is not available. Make sure you have included the script in the index.html file')
+}
+
 import { runDefaultExample } from '../app/examples/default.js'
 import { runSlotExample } from '../app/examples/slot.js'
 import { runDisabledExample } from '../app/examples/disabled.js'
--- postBuild.js
+++
@@ -1,3 +0,0 @@
-const fs = require('fs')
-
-fs.cpSync('dist', 'demo/static', { recursive: true })
