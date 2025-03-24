d505b2459581940133f2c9e0848e0f1c595cc4d2
Dzmitry Zhuraukou <zhuravkov888@gmail.com>
2025-03-24 16:18:21 +0100

    Revert "dev: fix demo page."
    This reverts commit 5447c828adb97886e7d8f270390b615e37968781.

--- app/examples/index.js
+++ app/examples/index.js
@@ -1,9 +1,9 @@
 import { runDefaultExample } from './default'
-import { runSlotExample } from './slot'
 import { runDisabledExample } from './disabled'
-import { runSingleSelectExample } from './singleSelect'
-import { runIndependentNodesExample } from './independentNodes'
 import { runIconsExample } from './icons'
+import { runIndependentNodesExample } from './independentNodes'
+import { runSingleSelectExample } from './singleSelect'
+import { runSlotExample } from './slot'
 
 export const runApp = (Treeselect) => {
   runDefaultExample(Treeselect)
--- demo/index.js
+++ demo/index.js
@@ -1,26 +1,10 @@
-import { runDefaultExample } from '../app/examples/default.js'
-import { runSlotExample } from '../app/examples/slot.js'
-import { runDisabledExample } from '../app/examples/disabled.js'
-import { runSingleSelectExample } from '../app/examples/singleSelect.js'
-import { runIndependentNodesExample } from '../app/examples/independentNodes.js'
-import { runIconsExample } from '../app/examples/icons.js'
+import { runApp } from '../app/examples'
 
 // Treeselect is available via the link in the index.html file (treeselectjs.umd.js)
 const Treeselect = globalThis.Treeselect
 
 if (!Treeselect) {
   throw new Error('Treeselect is not available. Make sure you have included the script in the index.html file')
-}
-
-const runApp = (Treeselect) => {
-  runDefaultExample(Treeselect)
-  runSlotExample(Treeselect)
-  runDisabledExample(Treeselect)
-  runSingleSelectExample(Treeselect)
-  runIndependentNodesExample(Treeselect)
-  runIconsExample(Treeselect)
-
-  document.body.classList.add('loaded')
 }
 
 runApp(Treeselect)
