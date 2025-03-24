dc3a9115e866ed20397ca016a94e51585c3cc26d
Dzmitry Zhuraukou <zhuravkov888@gmail.com>
2025-03-24 16:17:58 +0100

    Revert "dev: fix demo page."
    This reverts commit 116cef20de792d659738a9d5e086c6b7ed4a00f6.

--- app/app.js
+++ app/app.js
@@ -1,5 +1,5 @@
 import '../src/treeselectjs.css'
 import Treeselect from '../src/treeselectjs'
-import { runApp } from './examples/index.js'
+import { runApp } from './examples'
 
 runApp(Treeselect)
